cdc65d5b2ff1d7185cc3e3c9a1ca6cc40ed7328f
Oscar Gutierrez <98203302+iRetr0o@users.noreply.github.com>
2025-09-28 16:58:52 -0600

    feat: add model of shopping list

---
+++ Chopi/Models/ShoppingList.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingList.swift
+//  Chopi
+//
+//  Created by Oscar Gutierrez on 28/09/25.
+//
+
+import Foundation
+
+struct ShoppingList: Identifiable, Equatable {
+    let id: String
+    let name: String
+    let createdAt: Date
+}
