127a9fe2970a74931b22323e541346c12fb7fe17
Oscar Gutierrez <98203302+iRetr0o@users.noreply.github.com>
2025-09-28 14:42:05 -0600

    Initial Commit

---
+++ Chopi/ChopiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChopiApp.swift
+//  Chopi
+//
+//  Created by Oscar Gutierrez on 28/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ChopiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chopi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chopi
+//
+//  Created by Oscar Gutierrez on 28/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
