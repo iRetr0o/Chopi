3dd1882a69cdf8a12091edd0070f16aeba90584a
Oscar Gutierrez <98203302+iRetr0o@users.noreply.github.com>
2025-10-25 23:52:47 -0600

    test: add unit test to getItems

---
+++ ChopiTests/ListDetailViewModelTests.swift
@@ -0,0 +1,44 @@
+//
+//  ListDetailViewModelTests.swift
+//  ChopiTests
+//
+//  Created by Oscar Gutierrez on 25/10/25.
+//
+
+import XCTest
+@testable import Chopi
+
+final class ListDetailViewModelTests: XCTestCase {
+    var viewModel: ListDetailViewModel!
+    var mockDatabaseService: MockDatabaseService!
+    var stubDatabaseService: StubDatabaseService!
+    
+    override func setUp() {
+        super.setUp()
+        mockDatabaseService = MockDatabaseService()
+        stubDatabaseService = StubDatabaseService()
+    }
+    
+    override func tearDown() {
+        viewModel = nil
+        mockDatabaseService = nil
+        stubDatabaseService = nil
+        super.tearDown()
+    }
+    
+    func testGetItems() async {
+        let list = ShoppingList(id: "1", name: "Test List", createdAt: Date(), itemCount: 2)
+        viewModel = ListDetailViewModel(mockDatabaseService, shoppingList: list)
+        let expectation = XCTestExpectation(description: "Fetched Items from database")
+        
+        Task {
+            viewModel.getItems()
+            expectation.fulfill()
+        }
+        
+        await fulfillment(of: [expectation], timeout: 1.0)
+        
+        XCTAssertEqual(viewModel.items.count, 2)
+        XCTAssertFalse(viewModel.loading)
+    }
+}
